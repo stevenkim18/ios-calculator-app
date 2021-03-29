c2287bcfeb78fd743f8beecf8047fd1849502896
sjsj <boromir21@naver.com>
2021-03-29 14:09:26 +0900

    [Test] storyboard UI 구성 테스트

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -1,36 +1,116 @@
 //
 //  Calculator - ViewController.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var screenText: UILabel!
+    
+    @IBOutlet var dot: UIButton!
+    @IBOutlet var num0: UIButton!
+    @IBOutlet var num1: UIButton!
+    @IBOutlet var num2: UIButton!
+    @IBOutlet var num3: UIButton!
+    @IBOutlet var num4: UIButton!
+    @IBOutlet var num5: UIButton!
+    @IBOutlet var num6: UIButton!
+    @IBOutlet var num7: UIButton!
+    @IBOutlet var num8: UIButton!
+    @IBOutlet var num9: UIButton!
+    @IBOutlet var plusBtn: UIButton!
+    @IBOutlet var minusBtn: UIButton!
+    @IBOutlet var multiplyBtn: UIButton!
+    @IBOutlet var divideBtn: UIButton!
+    @IBOutlet var clearBtn: UIButton!
+    @IBOutlet var equalBtn: UIButton!
+    
+    var calculator = DecimalCalculator()
+    var printNum = 0
+    
+    @IBAction func numBtn(_ sender: UIButton) {
+        switch sender {
+        case plusBtn:
+            calculator.operandAndOperatorValidation(inputText: "+")
+        case minusBtn:
+            calculator.operandAndOperatorValidation(inputText: "-")
+        case multiplyBtn:
+            calculator.operandAndOperatorValidation(inputText: "*")
+        case divideBtn:
+            calculator.operandAndOperatorValidation(inputText: "/")
+        case equalBtn:
+            calculator.operandAndOperatorValidation(inputText: "=")
+        case clearBtn:
+            calculator.operandAndOperatorValidation(inputText: "clear")
+            screenText.text = "0"
+        default:
+            printNum = printNum * 10 + sender.tag
+            screenText.text = "\(printNum)"
+            calculator.operandAndOperatorValidation(inputText: String(screenText.text!))
+        }
+        screenText.text = "\(calculator.operandStack.peekLast()!)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let dCalc = DecimalCalculator()
-        dCalc.opperandAndOperatorValidation(inputText: "1100")
-        dCalc.opperandAndOperatorValidation(inputText: "+")
-        dCalc.opperandAndOperatorValidation(inputText: "100")
-        dCalc.opperandAndOperatorValidation(inputText: "*")
+        setup()
+    }
 
-        print(dCalc.opperandStack)
-        print(dCalc.operatorStack)
+    func setup() {
+        screenText.text = "0"
         
-        print("--------------------------------------------------")
+        num1.calcBtn()
+        num2.calcBtn()
+        num3.calcBtn()
+        num4.calcBtn()
+        num5.calcBtn()
+        num6.calcBtn()
+        num7.calcBtn()
+        num8.calcBtn()
+        num9.calcBtn()
+        dot.calcBtn()
         
+        plusBtn.operatorBtn()
+        minusBtn.operatorBtn()
+        multiplyBtn.operatorBtn()
+        divideBtn.operatorBtn()
+        clearBtn.operatorBtn()
+        equalBtn.operatorBtn()
         
-        let bCalc = BinaryCalculator()
-        bCalc.opperandAndOperatorValidation(inputText: "10")
-        bCalc.opperandAndOperatorValidation(inputText: "*")
-        bCalc.opperandAndOperatorValidation(inputText: "10")
-        bCalc.opperandAndOperatorValidation(inputText: "+")
-        bCalc.opperandAndOperatorValidation(inputText: "10")
-        bCalc.opperandAndOperatorValidation(inputText: "=")
+        dot.setTitle(".", for: .normal)
         
-        print(bCalc.operatorStack)
+        num0.setTitle("0", for: .normal)
+        num0.frame.size.width = 170
+        num0.frame.size.height = 60
+        num0.layer.cornerRadius = 30
+        num0.backgroundColor = .systemGray
+        num0.setTitleColor(.white, for: .normal)
+        num0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+
+}
+
+extension UIButton {
+    func calcBtn() {
+        self.setTitle("\(self.tag)", for: .normal)
+        self.frame.size.width = 60
+        self.frame.size.height = 60
+        self.layer.cornerRadius = self.frame.width / 2
+        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        self.backgroundColor = .systemGray
+    }
+    
+    func operatorBtn() {
+        self.frame.size.width = 60
+        self.frame.size.height = 60
+        self.layer.cornerRadius = self.frame.width / 2
+        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        self.backgroundColor = .systemOrange
     }
 }
--- Calculator/Calculator/Model/Calculrator.swift
+++ Calculator/Calculator/Model/Calculrator.swift
@@ -7,34 +7,6 @@
 
 import Foundation
 
-struct Deque<Element> {
-    var storage = Array<Element>()
-    
-    var count: Int {
-        return storage.count
-    }
-    
-    mutating func enque(element: Element) {
-        storage.append(element)
-    }
-    
-    mutating func deque() -> Element? {
-        return storage.removeFirst()
-    }
-    
-    mutating func clear() {
-        storage.removeAll()
-    }
-    
-    func peek() -> Element? {
-        return storage.first
-    }
-    
-    func isEmpty() -> Bool {
-        return storage.isEmpty
-    }
-}
-
 struct Stack<Element> {
     var storage = Array<Element>()
     
@@ -46,6 +18,10 @@ struct Stack<Element> {
         storage.append(element)
     }
     
+    mutating func popFirst() -> Element? {
+        return storage.removeFirst()
+    }
+    
     mutating func pop() -> Element? {
         return storage.popLast()
     }
@@ -54,7 +30,11 @@ struct Stack<Element> {
         storage.removeAll()
     }
     
-    func peek() -> Element? {
+    func peekFirst() -> Element? {
+        return storage.first
+    }
+    
+    func peekLast() -> Element? {
         return storage.last
     }
     
@@ -63,25 +43,56 @@ struct Stack<Element> {
     }
 }
 
+//struct Stack<Element> {
+//    var storage = Array<Element>()
+//
+//    var count: Int {
+//        return storage.count
+//    }
+//
+//    mutating func push(element: Element) {
+//        storage.append(element)
+//    }
+//
+//    mutating func pop() -> Element? {
+//        return storage.popLast()
+//    }
+//
+//    mutating func clear() {
+//        storage.removeAll()
+//    }
+//
+//    func peek() -> Element? {
+//        return storage.last
+//    }
+//
+//    func isEmpty() -> Bool {
+//        return storage.isEmpty
+//    }
+//}
+
 // 숫자를 누르면 숫자덱에, 연산자를 누르면 연산자덱에 enque
 // 연산자를 누르면 숫자덱의 개수를 확인하고 연산 후, 숫자덱에 연산결과 enque
 class DecimalCalculator {
-    var opperandStack = Deque<Int>()
-    var operatorStack = Deque<String>()
+    var operandStack = Stack<Int>()
+    var operatorStack = Stack<String>()
     
-    func opperandAndOperatorValidation(inputText: String) {
+    func operandAndOperatorValidation(inputText: String) {
         if let change = Int(inputText) {
-            opperandStack.enque(element: change)
+            operandStack.push(element: change)
         } else {
             switch inputText {
-            case "+", "-", "*", "/":
-                operatorStack.enque(element: inputText)
+            case "+", "-", "*", "/", "=":
+                operatorStack.push(element: inputText)
                 
-                if opperandStack.count <= 1 {
+                if operandStack.count <= 1 {
                     return
                 } else {
                     calculation()
                 }
+            case "clear":
+                operandStack.clear()
+                operatorStack.clear()
             default:
                 print("잘못된 입력 \(inputText)")
             }
@@ -89,23 +100,27 @@ class DecimalCalculator {
     }
     
     func calculation() {
-        let inputOperator = operatorStack.deque()
-        let opr1 = opperandStack.deque()
-        let opr2 = opperandStack.deque()
+        let inputOperator = operatorStack.popFirst()
+        guard let opr1 = operandStack.popFirst() else {
+            return
+        }
+        guard let opr2 = operandStack.popFirst() else {
+            return
+        }
         
         switch inputOperator {
         case "+":
-            let result = opr1! + opr2!
-            opperandStack.enque(element: result)
+            let result = opr1 + opr2
+            operandStack.push(element: result)
         case "-":
-            let result = opr1! - opr2!
-            opperandStack.enque(element: result)
+            let result = opr1 - opr2
+            operandStack.push(element: result)
         case "*":
-            let result = opr1! * opr2!
-            opperandStack.enque(element: result)
+            let result = opr1 * opr2
+            operandStack.push(element: result)
         case "/":
-            let result = opr1! / opr2!
-            opperandStack.enque(element: result)
+            let result = opr1 / opr2
+            operandStack.push(element: result)
         default:
             break
         }
@@ -115,59 +130,56 @@ class DecimalCalculator {
 
 // 숫자 버튼을 누르면 숫자스택에 push
 // 연산자를 누르면 연산자스택에 push
-class BinaryCalculator {
-    var opperandStack = Stack<Int>()
-    var operatorStack = Stack<String>()
-    
-    func opperandAndOperatorValidation(inputText: String) {
-        
-        if let change = Int(inputText) {
-            // 숫자일 경우 숫자스택에 push
-            opperandStack.push(element: change)
-        } else {
-            // 연산자일 경우
-            switch inputText {
-            case "+", "-", "*", "/":
-                if operatorStack.isEmpty() {
-                    // 연산자 스택이 비어있으면 push
-                    operatorStack.push(element: inputText)
-                } else {
-                    // 연산자 스택이 비어있지 않으면 우선 순위를 확인하고 push, pop
-                    let newValue = priorityComparison(value: inputText)
-                    let oldValue = priorityComparison(value: operatorStack.peek()!)
-                    
-                     if newValue <= oldValue {
-                        // oldValue의 우선순위가 높으면 연산자를 pop, 숫자를 두번 pop하고 연산 후 숫자 스택에 저장?
-                        operatorStack.push(element: inputText)
-                     } else {
-                        // oldValue의 우선순위가 낮으면 높은게 들어올때까지 스택에 push?
-                        operatorStack.push(element: inputText)
-                     }
-                }
-            case "=":
-                // 숫자 스택 두번 pop하고 연산자 스택 pop해서 연산하고 출력?
-            default:
-                break
-            }
-        }
-    }
-    
-    func priorityComparison(value: String) -> Int {
-        var resultValue = 0
-        
-        switch value {
-        case "+", "-":
-            resultValue = 1
-        case "*", "/":
-            resultValue = 2
-        default:
-            print("잘못된 입력\(value)")
-        }
-        
-        return resultValue
-    }
-    
-    func calculation() {
-        let inputOperator = op
-    }
-}
+//class BinaryCalculator {
+//    var operandStack = Stack<Int>()
+//    var operatorStack = Stack<String>()
+//
+//    func opperandAndOperatorValidation(inputText: String) {
+//
+//        if let change = Int(inputText) {
+//            // 숫자일 경우 숫자스택에 push
+//            operandStack.push(element: change)
+//        } else {
+//            // 연산자일 경우
+//            switch inputText {
+//            case "+", "-", "*", "/":
+//                if operatorStack.isEmpty() {
+//                    // 연산자 스택이 비어있으면 push
+//                    operatorStack.push(element: inputText)
+//                } else {
+//                    // 연산자 스택이 비어있지 않으면 우선 순위를 확인하고 push, pop
+//                    let newValue = priorityComparison(value: inputText)
+//                    let oldValue = priorityComparison(value: operatorStack.peek()!)
+//
+//                     if newValue <= oldValue {
+//                        // oldValue의 우선순위가 높으면 연산자를 pop, 숫자를 두번 pop하고 연산 후 숫자 스택에 저장?
+//                        operatorStack.push(element: inputText)
+//                     } else {
+//                        // oldValue의 우선순위가 낮으면 높은게 들어올때까지 스택에 push?
+//                        operatorStack.push(element: inputText)
+//                     }
+//                }
+//            case "=":
+//                break
+//                // 숫자 스택 두번 pop하고 연산자 스택 pop해서 연산하고 출력?
+//            default:
+//                break
+//            }
+//        }
+//    }
+//
+//    func priorityComparison(value: String) -> Int {
+//        var resultValue = 0
+//
+//        switch value {
+//        case "+", "-":
+//            resultValue = 1
+//        case "*", "/":
+//            resultValue = 2
+//        default:
+//            print("잘못된 입력\(value)")
+//        }
+//
+//        return resultValue
+//    }
+//}
